17947c130d0c5ec168d734c582e56ca8b2977fa8
karim <y2793623b@iespoblenou.org>
2017-03-24 16:54:03 +0100

    final

---
+++ Assets/Racket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour {
+	public float speed = 150.0f;
+
+	// Utilitzarem FixedUpdate quan  traballem en fisiques
+	void FixedUpdate () {
+		float horizontalDirection = Input.GetAxisRaw("Horizontal");
+
+			GetComponent<Rigidbody2D>().velocity = Vector2.right * horizontalDirection * speed;
+	}
+}
